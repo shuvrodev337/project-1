81aa5bda39f8c89008af047d3b8a286c61ca125d
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2024-12-04 13:26:34 -0800

    added middleware, routing

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,13 +5,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+//parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-//param
-app.get('/:id', (req, res) => {
-    console.log(req.params);
-    res.send('Hello developers!');
+// middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// creating routers
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+// creating base route
+app.use("/app/v1/users", userRouter);
+app.use("/app/v1/courses", courseRouter);
+// using routers
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
 });
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    res.json({
+        success: true,
+        message: "course is created successfully",
+        data: course,
+    });
+});
+// normal / get
+// app.get('/', (req: Request, res:Response) => {
+//     console.log(req.params);
+//   res.send('Hello developers!')
+// })
+//param
+// app.get('/:id', (req: Request, res:Response) => {
+//     console.log(req.params);
+//   res.send('Hello developers!')
+// })
 //multiple params
 // app.get('/:id/:subId', (req: Request, res:Response) => {
 //     console.log(req.params);
@@ -22,11 +56,16 @@ app.get('/:id', (req, res) => {
 //   console.log(req.query);
 //   res.send('Hello developers!')
 // })
-app.post('/', (req, res) => {
+// use of middleware
+// app.get('/',logger,  (req: Request, res:Response) => {
+//     res.send('Hello developers!')
+//   })
+// normal / post
+app.post("/", (req, res) => {
     console.log(req.body);
-    // res.send('response data') 
+    // res.send('response data')
     res.json({
-        message: "successfully receivd data"
+        message: "successfully receivd data",
     });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,53 @@
-import express, { Request, Response } from 'express'
-const app = express()
-app.use(express.json())
-app.use(express.text())
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
 
+//parsers
+app.use(express.json());
+app.use(express.text());
 
+// middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+// creating routers
+const userRouter = express.Router();
+const courseRouter = express.Router();
+// creating base route
+app.use("/app/v1/users", userRouter);
+app.use("/app/v1/courses", courseRouter);
+
+// using routers
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  res.json({
+    success: true,
+    message: "course is created successfully",
+    data: course,
+  });
+});
+
+// normal / get
+// app.get('/', (req: Request, res:Response) => {
+//     console.log(req.params);
+//   res.send('Hello developers!')
+// })
 
 //param
-app.get('/:id', (req: Request, res:Response) => {
-    console.log(req.params);
-  res.send('Hello developers!')
-})
+// app.get('/:id', (req: Request, res:Response) => {
+//     console.log(req.params);
+//   res.send('Hello developers!')
+// })
+
 //multiple params
 // app.get('/:id/:subId', (req: Request, res:Response) => {
 //     console.log(req.params);
@@ -22,14 +60,19 @@ app.get('/:id', (req: Request, res:Response) => {
 //   res.send('Hello developers!')
 // })
 
+// use of middleware
 
+// app.get('/',logger,  (req: Request, res:Response) => {
+//     res.send('Hello developers!')
+//   })
 
-app.post('/', (req: Request, res:Response) => {
-    console.log(req.body);
- // res.send('response data') 
- res.json({
-    message: "successfully receivd data"
- })
-})
+// normal / post
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  // res.send('response data')
+  res.json({
+    message: "successfully receivd data",
+  });
+});
 
 export default app;
